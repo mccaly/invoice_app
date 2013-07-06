5c7865621de0c2ab86b4b146fb20478363c7ccee
Andrew McCalister <andrewmccalister@googlemail.com>
2013-07-05 18:29:48 -0700

    sendgrid v6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,15 @@ InvoiceApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :login,
+  :user_name      => ENV['app12497979@heroku.com'],
+  :password       => ENV['9apum8hn'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 end
