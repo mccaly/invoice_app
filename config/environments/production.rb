fc1fd97665b277bc52da6aa14743afc2d992c189
Andrew McCalister <andrewmccalister@googlemail.com>
2013-07-05 18:37:28 -0700

    sendgrid v7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ InvoiceApp::Application.configure do
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :login,
-  :user_name      => ENV['app12497979@heroku.com'],
-  :password       => ENV['9apum8hn'],
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
