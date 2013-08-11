9d592ad6fde601aedb1a943e80e0b184782ced47
Richard Burton <burtonic@gmail.com>
2013-08-11 06:53:42 -0700

    Cosmetic changes to unit logic. Changed form type to inline.

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,33 +1,30 @@
 class UnitsController < ApplicationController
-
 	def create
-		@deal = Deal.find(params[:deal_id])
-		params[:unit][:amount] = (params[:unit][:amount].to_f * 100).to_i	
-		@unit = Unit.create(params[:unit])
-		@invoice = @deal.invoices.where(status: "Active").first
-		#@invoice = Invoice.find_by(@deal.id == :deal_id).where(status: "Active")
-		@unit.deal = @deal
+		@deal                  = Deal.find(params[:deal_id])
+		params[:unit][:amount] = (params[:unit][:amount].to_f * 100).to_i
+		@unit                  = Unit.create(params[:unit])
+		@invoice               = @deal.invoices.where(status: "Active").first
+		#@invoice              = Invoice.find_by(@deal.id == :deal_id).where(status: "Active")
+		@unit.deal             = @deal
 		@unit.save
-		if @unit.save && @invoice		
-				unit_tally = UnitTally.create
-				unit_tally.name = @unit.name
-				unit_tally.access_token = @unit.access_token
-				unit_tally.status = 'Active'
-				unit_tally.end_date = @invoice.end_date
-				unit_tally.set(:total, unit_tally.tallys.sum(:amount_total).to_i)
-				unit_tally.invoice = @invoice
-				unit_tally.unit = @unit
-				unit_tally.save
-				@invoice.unit_tallys << unit_tally
-				@invoice.save
+
+		if @unit.save && @invoice
+			unit_tally              = UnitTally.create
+			unit_tally.name         = @unit.name
+			unit_tally.access_token = @unit.access_token
+			unit_tally.status       = 'Active'
+			unit_tally.end_date     = @invoice.end_date
+			unit_tally.set(:total, unit_tally.tallys.sum(:amount_total).to_i)
+			unit_tally.invoice      = @invoice
+			unit_tally.unit         = @unit
+			unit_tally.save
+			@invoice.unit_tallys << unit_tally
+			@invoice.save
 			flash[:success] = "New Unit saved"
 			redirect_to deal_path(@deal)
-		#else
-			#raise unit.errors.full_messages.inspect
 		else
 			redirect_to deal_path(@deal)
 		end
-		
 	end
 
 	def new
@@ -58,8 +55,8 @@ class UnitsController < ApplicationController
 	def update
 	@deal = Deal.find(params[:deal_id])
 	@unit = Unit.find(params[:id])
-	tally = Tally.where(:access_token => @unit.access_token).and(:date => Date.today).first
-	params[:unit][:amount] = (params[:unit][:amount].to_f * 100).to_i	
+	tally = Tally.where(access_token: @unit.access_token).and(date: Date.today).first
+	params[:unit][:amount] = (params[:unit][:amount].to_f * 100).to_i
 		if @unit.update_attributes(params[:unit])
 			if tally
 				tally.update_attributes(amount_unit: @unit.amount)
@@ -69,6 +66,6 @@ class UnitsController < ApplicationController
 			redirect_to deal_path(@deal)
 		else
 			render 'edit'
-		end	
+		end
 	end
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,18 +1,15 @@
-class Unit 
+class Unit
 	include Mongoid::Document
   include Mongoid::Timestamps
 
+  before_create :generate_access_token
 
-before_create :generate_access_token
-
-  field :access_token, :type => String
-  field :name, :type => String
-  field :amount, :type => Integer
-
+  field :access_token, type: String
+  field :name,         type: String
+  field :amount,       type: Integer
 
   belongs_to :deal
-
-  has_many :unit_tallys
+  has_many   :unit_tallys
 
  private
 
