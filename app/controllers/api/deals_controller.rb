9f5b11757212f58e3911b2e230fb6b6c43bb8ee6
Gordon Murray Dent <gordonmdent@gmail.com>
2013-08-29 17:01:20 +0000

    complete deals api controller

--- app/controllers/api/deals_controller.rb
+++ app/controllers/api/deals_controller.rb
@@ -20,17 +20,30 @@ class Api::DealsController < Api::ApiController
 
   def get
     deals = current_user.accounts.find(params[:account_id]).deals
-    if account
-      render json: deals.find(params[:id])
+    deal = deals.find(params[:id])
+    if deal
+      render json: deal
     else 
       render nothing: true, status: 404
     end
   end
 
   def update
-    
+    deals = current_user.accounts.find(params[:account_id]).deals
+    deal = deals.find(params[:id])
+    if deal
+      if deal.update_attributes(params[:deal])
+        render json: deal
+      else
+        render json: false, status: 400
+      end
+    else
+      render nothing: true, status: 404
+    end
   end
 
   def delete
+    account = current_user.accounts.find(params[:id])
+    render json: account.destroy
   end
 end
