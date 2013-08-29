ebc769e5a476765f2c2385cfa49800ee5aa087bb
Gordon Murray Dent <gordonmdent@gmail.com>
2013-08-29 17:02:23 +0000

    add other generated files

---
+++ spec/controllers/api/deals_controller_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Api::DealsControllerController do
+
+  describe "GET 'create'" do
+    it "returns http success" do
+      get 'create'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'collection'" do
+    it "returns http success" do
+      get 'collection'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'get'" do
+    it "returns http success" do
+      get 'get'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'update'" do
+    it "returns http success" do
+      get 'update'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'delete'" do
+    it "returns http success" do
+      get 'delete'
+      response.should be_success
+    end
+  end
+
+end
