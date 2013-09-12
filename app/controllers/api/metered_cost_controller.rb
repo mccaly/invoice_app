3b17d21936436b8093be6a6b346e07b5e995e24e
Gordon Murray Dent <gordonmdent@gmail.com>
2013-09-12 21:03:53 +0000

    finish fleshing out metered cost controller functionality

--- app/controllers/api/metered_cost_controller.rb
+++ app/controllers/api/metered_cost_controller.rb
@@ -7,10 +7,29 @@ class Api::MeteredCostController < ApplicationController
         params[:metered][:amount] = (params[:metered][:amount].to_f * 100).to_i
         metered = deal.units.build(params[:metered])
         deal.units << metered
-        render json: metered 
+        self.create_invoice_metered_cost_tally(deal,metered)
+        return render json: metered 
       end
     else
       render nothing: true, status: 404
+    end
+  end
+
+  protected
+  def create_invoice_metered_cost_tally(deal, metered)
+    invoice = deal.invoices.where(status: "Active").first
+    if invoice
+      metered_tally              = UnitTally.create
+      metered_tally.name         = metered.name
+      metered_tally.access_token = metered.access_token
+      metered_tally.status       = 'Active'
+      metered_tally.end_date     = @invoice.end_date
+      metered_tally.set(:total, metered_tally.tallys.sum(:amount_total).to_i)
+      metered_tally.invoice      = @invoice
+      metered_tally.unit         = metered
+      metered_tally.save
+      invoice.unit_tallys << metered_tally
+      invoice.save
     end
   end
 
@@ -19,16 +38,34 @@ class Api::MeteredCostController < ApplicationController
   end
 
   def get
-    deals = current_user.accounts.find(params[:account_id]).deals
-    deal = deals.find(params[:id])
-    if deal
-      render json: deal
-    else 
-      render nothing: true, status: 404
+    ccount = current_user.accounts.find(params[:account_id])
+    if account
+      deal = account.deals.find(params[:deal_id])
+      if deal 
+        metered = deal.units.find(params[:metered_id])
+        if metered
+          return render json: metered
+        end
+      end
     end
+
+    render nothing: true, status: 404
   end
 
   def update
+    ccount = current_user.accounts.find(params[:account_id])
+    if account
+      deal = account.deals.find(params[:deal_id])
+      if deal 
+        metered = deal.units.find(params[:metered_id])
+        if metered
+          metered.update_attributes(params[:metered])
+          return render json: metered
+        end
+      end
+    end
+
+    render nothing: true, status: 404
   end
 
   def delete
@@ -36,9 +73,9 @@ class Api::MeteredCostController < ApplicationController
     if account
       deal = account.deals.find(params[:deal_id])
       if deal 
-        metered = deal.metered.find(params[:metered_id])
+        metered = deal.units.find(params[:metered_id])
         if metered
-          render json: metered.destroy
+          return render json: metered.destroy
         end
       end
     end
