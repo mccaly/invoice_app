58161ecb20fd3cf438a6aef3a2db76a2dd7943a4
Gordon Murray Dent <gordonmdent@gmail.com>
2013-09-26 18:40:22 +0000

    add metered cost api

--- app/controllers/api/metered_cost_controller.rb
+++ app/controllers/api/metered_cost_controller.rb
@@ -1,4 +1,43 @@
 class Api::MeteredCostController < Api::ApiController
+
+  def tally
+    unit = Unit.find_by(access_token: params[:access_token])
+    unit_tally = unit.unit_tallys.where(status: 'Active').first
+    invoice = unit_tally.invoice
+    tally = Tally.where(:access_token => unit.access_token).and(:date => Date.today).first
+    if tally
+      tally.inc(:quantity, 1)
+      tally_running_total = (tally.amount_unit).to_i
+      tally.inc(:amount_total, tally_running_total) 
+      unit_tally.inc(:total, tally.amount_unit)
+      invoice.set(:metered_total, invoice.unit_tallys.sum(:total).to_i)
+      if invoice.adjust_total != "true"
+        invoice.set(:amount, invoice.metered_total + invoice.basecost_total)
+      end
+
+      return render json: tally
+    else
+      tally_new = Tally.create(params[:tally])
+      tally_new.unit_tally = unit_tally
+      tally_new.quantity = 1
+      tally_new.date = Date.today
+      tally_new.amount_unit = unit.amount
+      tally_new.amount_total = (@tally_new.amount_unit * @tally_new.quantity)
+      tally_new.access_token = unit.access_token
+      tally_new.deal = unit.deal
+      unit_tally.tallys << tally_new
+      unit_tally.inc(:total, tally_new.amount_unit)
+      unit_tally.save
+      invoice.set(:metered_total, invoice.unit_tallys.sum(:total).to_i)
+      if invoice.adjust_total != "true"
+        invoice.set(:amount, invoice.metered_total + invoice.basecost_total)
+      end
+      tally_new.save
+
+      return render json: tally_new
+    end 
+  end
+
   def create
     account = current_user.accounts.find(params[:account_id])
     if account
